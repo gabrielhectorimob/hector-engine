b582480cefdd02b6dc9c357c3041b8d6a92d3c92
gabrielhectorimob <gabriel.mge@gmail.com>
2026-03-09 15:12:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Question(BaseModel):
+    question: str
 
 @app.get("/")
 def root():
     return {"message": "Hector Engine running"}
+
+@app.post("/query")
+def query(q: Question):
+    return {
+        "answer": f"Hector recebeu a pergunta: {q.question}"
+    }
 
 @app.get("/health")
 def health():
